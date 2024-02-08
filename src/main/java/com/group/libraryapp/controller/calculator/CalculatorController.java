5b9a5b4d903de8e28edfa3ac60eb279a284c6413
Jihyeok <qowlgur121@gmail.com>
2024-02-09 02:19:17 +0900

    Add 사용자 등록 기능

--- src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -1,13 +1,24 @@
 package com.group.libraryapp.controller.calculator;
 
 import com.group.libraryapp.dto.calculator.request.CalculatorAddRequest;
+import com.group.libraryapp.dto.calculator.request.CalculatorMultiplyRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController //현재 클래스를 api의 진입지점(Controller)으로 설정하는 어노테이션
+@RestController
 public class CalculatorController {
-    @GetMapping("/add") // Get /add
+
+    @GetMapping("/add") // GET /add
     public int addTwoNumbers(CalculatorAddRequest request) {
         return request.getNumber1() + request.getNumber2();
     }
+
+    @PostMapping("/multiply") // POST /multiply
+    public int multiplyTwoNumbers(@RequestBody CalculatorMultiplyRequest request) {
+        return request.getNumber1() * request.getNumber2();
+    }
+
 }
+
